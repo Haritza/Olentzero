1f82a48124001fb820e583170af3f3057d6067a9
Andoni <andoni.correas@gmail.com>
2015-12-04 21:43:09 +0100

    typo

--- begiak.py
+++ begiak.py
@@ -7,8 +7,8 @@ class Begiak:
     def __init__(self,begien_posizioa):
         self.ezkerreko_begia = begien_posizioa[0]
         self.eskuineko_begia = begien_posizioa[1]
-        __pwd.setPWM(self.ezkerreko_begia,0,__servoMin)
-        __pwd.setPWM(self.eskuineko_begia,0,__servoMax)
+        __pwm.setPWM(self.ezkerreko_begia,0,__servoMin)
+        __pwm.setPWM(self.eskuineko_begia,0,__servoMax)
 
     def mugitu_begiak_hasieran(self):
         __pwm.setPWM(self.ezkerreko_begia, 0, __servoMax)
