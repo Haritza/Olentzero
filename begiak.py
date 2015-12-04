1c712aa91026dccf6940bdd5d787fcfd2fe6a831
Andoni <andoni.correas@gmail.com>
2015-12-04 21:49:01 +0100

    self gehituta..

--- begiak.py
+++ begiak.py
@@ -6,18 +6,18 @@ class Begiak:
         self.ezkerreko_begia = begien_posizioa[0]
         self.eskuineko_begia = begien_posizioa[1]
 
-        pwm = PWM(0x40)
-        servoMin = 150
-        servoMax = 600
+        self.pwm = PWM(0x40)
+        self.servoMin = 150
+        self.servoMax = 600
 
-        pwm.setPWM(self.ezkerreko_begia,0,servoMin)
-        pwm.setPWM(self.eskuineko_begia,0,servoMax)
+        self.pwm.setPWM(self.ezkerreko_begia,0,self.servoMin)
+        self.pwm.setPWM(self.eskuineko_begia,0,self.servoMax)
 
     def mugitu_begiak_hasieran(self):
-        pwm.setPWM(self.ezkerreko_begia, 0, servoMax)
-        pwm.setPWM(self.eskuineko_begia, 0, servoMax)
+        self.pwm.setPWM(self.ezkerreko_begia, 0, self.servoMax)
+        self.pwm.setPWM(self.eskuineko_begia, 0, self.servoMax)
 
 
     def mugitu_begiak_bukaeran(self):
-        pwm.setPWM(self.ezkerreko_begia, 0, servoMin)
-        pwm.setPWM(self.eskuineko_begia, 0, servoMin)
+        self.pwm.setPWM(self.ezkerreko_begia, 0, self.servoMin)
+        self.pwm.setPWM(self.eskuineko_begia, 0, self.servoMin)
