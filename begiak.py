1ce9750d90c27d99e5df25e9767851cbdf0f4e28
Andoni <andoni.correas@gmail.com>
2015-12-04 21:45:56 +0100

    ...

--- begiak.py
+++ begiak.py
@@ -1,20 +1,23 @@
 from Adafruit_PWM_Servo_Driver import PWM
 
 class Begiak:
-    __pwm = PWM(0x40)
-    __servoMin = 150
-    __servoMax = 600
+
     def __init__(self,begien_posizioa):
         self.ezkerreko_begia = begien_posizioa[0]
         self.eskuineko_begia = begien_posizioa[1]
-        __pwm.setPWM(self.ezkerreko_begia,0,__servoMin)
-        __pwm.setPWM(self.eskuineko_begia,0,__servoMax)
+
+        pwm = PWM(0x40)
+        servoMin = 150
+        servoMax = 600
+
+        pwm.setPWM(self.ezkerreko_begia,0,servoMin)
+        pwm.setPWM(self.eskuineko_begia,0,servoMax)
 
     def mugitu_begiak_hasieran(self):
-        __pwm.setPWM(self.ezkerreko_begia, 0, __servoMax)
-        __pwm.setPWM(self.eskuineko_begia, 0, __servoMax)
+        pwm.setPWM(self.ezkerreko_begia, 0, servoMax)
+        pwm.setPWM(self.eskuineko_begia, 0, servoMax)
 
 
     def mugitu_begiak_bukaeran(self):
-        __pwm.setPWM(self.ezkerreko_begia, 0, __servoMin)
-        __pwm.setPWM(self.eskuineko_begia, 0, __servoMin)
+        pwm.setPWM(self.ezkerreko_begia, 0, servoMin)
+        pwm.setPWM(self.eskuineko_begia, 0, servoMin)
